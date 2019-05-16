03ffea7217fad347d4baa8c5ca6800b039252e33
Naveendubey93 <36043723+Naveendubey93@users.noreply.github.com>
2019-05-16 22:37:57 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//import {}
-//const routes: Routes = [];
+import { BaseModule } from './modules/base/base.module';
+import { UserModule } from './modules/user/user.module';
+
 const routes: Routes = [
 {
-    path: '',redirectTo: "/user/login",pathMatch: 'full'
-}
-
+    path: '', pathMatch: 'full', redirectTo: 'user/login'
+},
+{
+ 	path: 'user/login', component: UserModule 
+},
+{
+	path: '', loadChildren:'user/user.module#UserRoutingModule'
+},
 ];
 
 @NgModule({
